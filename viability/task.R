71fa0340767d88b43124be62d45f95f63c95c31c
Jan-Philipp Cieslik <github@cieslik.io>
2023-07-02 00:04:55 +0200

    minor

---
+++ viability/task.R
@@ -0,0 +1,15 @@
+#load data from data/sanger.csv into a variable called "data"
+
+
+#create a subset called MCF7 and MDA
+#containing only data from MCF7 or MDA-MB-231
+
+
+#subset the data further to only contain TAMOXIFEN data with a dose of 5
+#call the new variables MCF7_Tamoxifen and MDA_Tamoxifen
+
+#calculate the mean "viability" difference between the two groups previously defined
+#assign the result to viability_diff
+
+#perform a t test using t.test() between the two groups previously defined
+#save the response to viability_ttest
