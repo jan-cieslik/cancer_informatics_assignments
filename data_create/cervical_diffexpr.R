0bef927745c8bf31e7577349542435a8c457ea90
dilara-kpl <124679411+dilara-kpl@users.noreply.github.com>
2023-08-01 18:04:15 +0200

    Added cervical_diffexpr.R

---
+++ data_create/cervical_diffexpr.R
@@ -0,0 +1,4 @@
+#downloaded from NBLDA https://CRAN.R-project.org/package=NBLDA
+#original data is from Witten, D., et al. https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2880020/
+library(NBLDA)
+data(cervical)
