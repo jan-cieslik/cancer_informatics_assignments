39cb0bb968987535e5e3905a86bba7151c867704
dilara-kpl <124679411+dilara-kpl@users.noreply.github.com>
2023-08-31 13:17:41 +0200

    Create cervsurv_task.R

---
+++ cervsurv/cervsurv_task.R
@@ -0,0 +1,20 @@
+#load the survival & survminer packages
+
+
+
+#load the dataset
+
+
+#create a Surv object called "sobj"
+
+
+#fit the Kaplan-Meier curves & assign the result to "km"
+
+
+#create a Kaplan-Meier plot with number at risk & assign the result to "km_plot"
+
+
+#fit the Kaplan-Meier curves with covariates & assign the result to "km_type"
+
+
+#create a Kaplan-Meier plot with number at risk and p-value & assign the result to "km_type_plot"
