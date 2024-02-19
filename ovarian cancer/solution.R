4893f24d1cdd37651a59ca9d1952262bbbdf843a
Jan-Philipp Cieslik <github@cieslik.io>
2024-02-19 19:29:55 +0100

    push

--- ovarian cancer/solution.R
+++ ovarian cancer/solution.R
@@ -16,7 +16,7 @@ print(column_names)
 library(survival) 
 
 # Task 5: Create a Kaplan-Meier survival curve and save the data as km_fit
-km_fit <- survfit(Surv(Time, Status) ~ Treatment, data = simulated_data)
+km_fit <- survminer::surv_fit(Surv(Time, Status) ~ Treatment, data = simulated_data)
 
 # Task 6: Extract the number of participans from the Kaplan-Meier curve
 num_participants <- km_fit$n
@@ -33,13 +33,13 @@ plot(km_fit, col = c("red", "blue"), lwd = 2, xlab = "Time (Months)", ylab = "Su
      main = "Kaplan-Meier Survival Curve", xlim = c(0, 36), ylim = c(0, 1),
      xaxs = "i", yaxs = "i", las = 1)
 
-library(survival)
+library(survminer)
 
 p <- ggsurvplot(km_fit)
 
 # Task 9: Create a legend in the bottom-right corner of the plot. 
 # Label the treatment group as "Treatment" and the no treatment group as "No Treatment". 
 # Use the colors blue and red for the respective groups.
-legend("bottomright", legend = c("Treatment", "No Treatment"), col = c("blue", "red"), lwd = 2)
+#legend("bottomright", legend = c("Treatment", "No Treatment"), col = c("blue", "red"), lwd = 2)
 
 # Task 10: Cox Regression + Boxplot + HR's
